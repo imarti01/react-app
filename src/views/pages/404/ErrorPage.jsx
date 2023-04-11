e818c1d9851ce3898cd9aea984d794dd05d4129a
Isaura Martí <imarti.teixido@gmail.com>
2023-04-11 12:59:14 +0200

    feat 404 page

--- src/views/pages/404/ErrorPage.jsx
+++ src/views/pages/404/ErrorPage.jsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+import "./ErrorPage.scss";
+
 export const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="error-page">
+      <h3>Error code: 404</h3>
+      <h2>Page not found...</h2>
+      <Link to="/">Go to Main Page</Link>
+    </div>
+  );
 };
